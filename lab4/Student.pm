ba22650cb4ba738f58a634dec4e284d2429db330
Unlucked01 <unlucked@yandex.ru>
2024-10-04 10:38:55 +0300

    lab4

---
+++ lab4/Student.pm
@@ -0,0 +1,91 @@
+package Student;
+
+sub new {
+    my ($class, $id, $fio, $group, $speciality, $birth_year, @grades) = @_;
+    my $self = {
+        id         => $id,
+        fio        => $fio,
+        group      => $group,
+        speciality => $speciality,
+        birth_year => $birth_year,
+        grades     => \@grades,  # Массив оценок
+        next       => undef      # Ссылка на следующий элемент списка
+    };
+    bless $self, $class;
+    return $self;
+}
+
+sub DESTROY {
+    my $self = shift;
+    print "Студент с номером зачетной книжки $self->{id} удален.\n";
+}
+
+sub get_average {
+    my ($self) = @_;
+    my $grades = $self->{grades};
+    return 0 unless @$grades;  # Если нет оценок, средний балл = 0
+    my $sum = 0;
+    $sum += $_ for @$grades;
+    return $sum / @$grades;
+}
+
+sub compare {
+    my ($student1, $student2) = @_;
+    my $avg1 = $student1->get_average;
+    my $avg2 = $student2->get_average;
+    
+    if ($avg1 > $avg2) {
+        return "$student1->{fio} (с баллом $avg1) учится лучше, чем $student2->{fio} (с баллом $avg2).\n";
+    } elsif ($avg1 < $avg2) {
+        return "$student2->{fio} (с баллом $avg1) учится лучше, чем $student1->{fio} (с баллом $avg2).\n";
+    } else {
+        return "$student1->{fio} (с баллом $avg1) и $student2->{fio} (с баллом $avg2) учатся одинаково хорошо.\n";
+    }
+}
+
+sub append {
+    my ($self, $new_node) = @_;
+    
+    if (!$self || $new_node->{id} <= $self->{id}) {
+        $new_node->{next} = $self;
+        return $new_node;
+    }
+
+    $self->{next} = append($self->{next}, $new_node); 
+    return $self;
+}
+
+sub my_delete {
+    my ($self, $value) = @_;
+    if (!$self) {
+        return undef;
+    }
+    if ($self->{id} == $value) {
+    	my $next = $self->{next};
+        undef $self;  # Здесь вызовется деструктор для текущего объекта
+        return $next;
+    }
+    $self->{next} = $self->{next}->my_delete($value) if $self->{next};
+    return $self;
+}
+
+sub find_student {
+    my ($self, $id) = @_;
+    my $current = $self;
+    while ($current) {
+        return $current if $current->{id} == $id;
+        $current = $current->{next};
+    }
+    return undef;
+}
+
+sub print_list {
+    my ($self) = @_;
+    my $current = $self;
+    while ($current) {
+        print "Номер зачетной книжки: $current->{id} ФИО: $current->{fio} Группа: $current->{group} Специальность: $current->{speciality} Год рождения: $current->{birth_year} Оценки: " . join(", ", @{$current->{grades}}) . "\n";
+        $current = $current->{next};
+    }
+}
+
+1;  # Конец модуля
---
+++ lab4/main.pl
@@ -0,0 +1,63 @@
+#!/usr/bin/perl
+
+use lib '.';
+use Student;
+
+my $head = undef;
+
+while (1) {
+    print "\nВыберите операцию:\n";
+    print "1. Добавить студента\n";
+    print "2. Удалить студента\n";
+    print "3. Вывести список студентов\n";
+    print "4. Сравнить студентов\n";
+    print "5. Выйти\n";
+    print "Ваш выбор: ";
+    
+    chomp(my $choice = <>);
+
+    if ($choice == 1) {
+        print "Введите номер зачетной книжки: ";
+        chomp(my $id = <>);
+        print "Введите ФИО: ";
+        chomp(my $fio = <>);
+        print "Введите название группы: ";
+        chomp(my $group = <>);
+        print "Введите специальность: ";
+        chomp(my $spec = <>);
+        print "Введите год рождения: ";
+        chomp(my $birth = <>);
+        print "Введите оценки через пробел: ";
+        chomp(my $grades_input = <>);
+        my @grades = split(' ', $grades_input);  # Разделяем оценки
+        my $student = Student->new($id, $fio, $group, $spec, $birth, @grades);
+
+        $head = $head ? $head->append($student) : $student;
+        
+        print "Добавление выполнено.\n";
+
+    } elsif ($choice == 2) {
+        print "Введите номер зачетной книжки для удаления: ";
+        chomp(my $value = <>);
+        $head = $head ? $head->my_delete($value) : undef;
+    } elsif ($choice == 3) {
+    	$head ? $head->print_list() : print "Список студентов пуст.\n";
+    } elsif ($choice == 4) {
+        print "Введите номер зачетной книжки первого студента: ";
+        chomp(my $id1 = <>);
+        print "Введите номер зачетной книжки второго студента: ";
+        chomp(my $id2 = <>);
+        my $student1 = $head->find_student($id1) if $head;
+        my $student2 = $head->find_student($id2) if $head;
+        if ($student1 && $student2) {
+            print Student::compare($student1, $student2);
+        } else {
+            print "Один из студентов не найден.\n";
+        }
+    } elsif ($choice == 5) {
+        print "Выход\n";
+        last;
+    } else {
+        print "Такой опции не существует\n";
+    }
+}
