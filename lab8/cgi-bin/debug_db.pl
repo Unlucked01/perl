9e1ce38311523f807d4a6a459e7c5c3731d956f5
Egor Kopylov <113236909+Unlucked01@users.noreply.github.com>
2025-03-05 20:51:00 +0300

    small encode fix

--- lab8/cgi-bin/debug_db.pl
+++ lab8/cgi-bin/debug_db.pl
@@ -5,7 +5,6 @@ use utf8;
 use DB_File;
 use Encode qw(decode encode);
 
-# Set output to UTF-8
 binmode(STDOUT, ":utf8");
 print "Content-type: text/plain; charset=utf-8\n\n";
 
@@ -13,10 +12,11 @@ my %dictionary;
 tie %dictionary, 'DB_File', 'dictionary.db', O_RDONLY, 0666, $DB_HASH
     or die "Cannot open dictionary.db: $!";
 
-print "Dictionary Contents:\n";
-print "====================\n";
+print "\nDictionary Contents (Decoded):\n";
+print "==============================\n";
 foreach my $key (sort keys %dictionary) {
-    print "Key: '$key', Value: '$dictionary{$key}'\n";
+    my $value = decode('UTF-8', $dictionary{$key}, Encode::FB_CROAK);
+    print "Key: '$key', Value: '$value'\n";
 }
 
 untie %dictionary; 
